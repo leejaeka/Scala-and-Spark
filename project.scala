20051cb6dbd7d11d72926c07c0c43256705a1d39
Jaekang Lee <jaekang.lee@mail.utoronto.ca>
2020-10-08 21:40:06 -0700

    Update project.scala

--- project.scala
+++ project.scala
@@ -16,27 +16,31 @@ for (col <- df.columns){
 // What does the Schema look like?
 df.describe().show()
 // Print out the first 5 columns.
-df.first(5).show()
 // Use describe() to learn about the DataFrame.
 
 // Create a new dataframe with a column called HV Ratio that
 // is the ratio of the High Price versus volume of stock traded
 // for a day.
-
+//val df2 = df.withColumn("HV Ratio", df("High")/df("Volume"))
 // What day had the Peak High in Price?
-
+//df.select(max("High")).show()
 // What is the mean of the Close column?
-
+//df.select(mean("Close")).show()
 // What is the max and min of the Volume column?
-
+//df.select(max("Volume"),min("Volume")).show()
 // For Scala/Spark $ Syntax
 
 // How many days was the Close lower than $ 600?
-
+df.filter($"Close"< 600).count()
 // What percentage of the time was the High greater than $500 ?
-
+df.groupBy($"High">500).count()/len(df.select("High))
 // What is the Pearson correlation between High and Volume?
-
+//df.select(corr("High","Low")).show()
 // What is the max High per year?
-
+val df2 = df.withColumn("Year", year(df("Date")))
+val dfavgs = df2.groupBy("Year").max()
+dfavgs.select($"Year",$"max(High)").show()
 // What is the average Close for each Calender Month?
+val df3 = df.withColumn("Month", month(df("Date")))
+val dfavgs = df3.groupBy("Month").mean()
+dfavgs.select($"Month",$"avg(Close)").orderBy("Month").show()
